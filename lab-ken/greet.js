ea34f6fbed842cac4f8e0666162ba408a69e6b92
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2017-01-02 20:14:11 -0800

    got functions working

---
+++ lab-ken/greet.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const greet = require ('./lib/greet.js');
+const arithmetic = require('./lib/arithmetic.js');
+
+console.log(greet('world'));
+console.log(arithmetic.add(2,2));
+console.log(arithmetic.add(5,5));
+console.log(arithmetic.add(7,7));
+
+console.log(arithmetic.sub(10,5));
+console.log(arithmetic.sub(5,5));
+console.log(arithmetic.sub(2,1));
---
+++ lab-ken/lib/arithmetic.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const arithmetic = module.exports = {};
+
+arithmetic.add = function(a,b){
+
+    return a + b;
+}
+
+arithmetic.sub = function(a,b){
+  return a - b;
+}
