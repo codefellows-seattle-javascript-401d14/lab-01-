a5afcf71a87dc6470d55dec1a8e73d548f295646
devonhackley <devonhackley@gmail.com>
2017-01-04 07:45:12 -0800

    included my index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const greet = require('./lib/greet.js');
+const arithmetic = require('./lib/arithmetic.js');
+
+console.log(greet('Devon'));
+
+console.log(arithmetic.add(2,3));
+console.log(arithmetic.sub(7,5));
